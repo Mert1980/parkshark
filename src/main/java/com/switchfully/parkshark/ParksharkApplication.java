80542813576d88a724cf569d736a59b54a7fe786
Thomas De Maesschalck <thomas.dm@dvhardware.net>
2021-11-16 22:24:49 +0100

    Updated SQL schema

--- src/main/java/com/switchfully/parkshark/ParksharkApplication.java
+++ src/main/java/com/switchfully/parkshark/ParksharkApplication.java
@@ -1,6 +1,5 @@
 package com.switchfully.parkshark;
 
-import java.time.LocalDate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/resources/Parkshark.sql
+++ src/main/resources/Parkshark.sql
@@ -1,85 +1,101 @@
-SET SCHEMA 'parkshark';
-
-CREATE TYPE "parking_category" AS ENUM (
-  'underground',
-  'aboveground'
+create table parkshark.division
+(
+    division_id        integer not null
+        constraint division_pk
+            primary key,
+    parent_division_id integer,
+    name               varchar,
+    original_name      varchar,
+    director_id        integer
 );
 
-CREATE TYPE "membership_type" AS ENUM (
-  'Bronze',
-  'Silver',
-  'Gold'
+alter table parkshark.division
+    owner to student;
+
+create table parkshark.address
+(
+    address_id   integer not null
+        constraint address_pk
+            primary key,
+    streetname   varchar,
+    streetnumber varchar,
+    city         varchar,
+    postal_code  varchar
 );
 
-CREATE TABLE "division" (
-  "division_id" int,
-  "parent_division_id" int,
-  "name" varchar,
-  "original_name" varchar,
-  "director_id" int
+alter table parkshark.address
+    owner to student;
+
+create table parkshark.person
+(
+    id                   integer not null
+        constraint person_pk
+            primary key,
+    firstname            varchar,
+    lastname             varchar,
+    email                varchar,
+    address_id_fk        integer
+        constraint address_id_fk
+            references parkshark.address,
+    phonenumber_mobile   varchar,
+    phonenumber_local    varchar,
+    license_plate_number varchar,
+    registration_date    date
 );
 
-CREATE TABLE "parkinglot" (
-  "name" varchar,
-  "parking_category" parking_category,
-  "price_per_hour" decimal,
-  "capacity" int,
-  "address_id_fk" int,
-  "division_id_fk" int,
-  "contact_id_fk" int,
-  "parkinglot_id" int
+alter table parkshark.person
+    owner to student;
+
+create table parkshark.parkinglot
+(
+    name             varchar,
+    parking_category parkshark.parking_category,
+    price_per_hour   numeric,
+    capacity         integer,
+    address_id_fk    integer
+        constraint address_id_fk
+            references parkshark.address,
+    division_id_fk   integer
+        constraint division_id_fk
+            references parkshark.division,
+    contact_id_fk    integer
+        constraint contact_id_fk
+            references parkshark.person,
+    parkinglot_id    integer not null
+        constraint parkinglot_pk
+            primary key
 );
 
-CREATE TABLE "person" (
-  "id" int,
-  "firstname" varchar,
-  "lastname" varchar,
-  "email" varchar,
-  "address_id_fk" int,
-  "phonenumber_mobile" varchar,
-  "phonenumber_local" varchar,
-  "license_plate_number" varchar,
-  "registration_date" date
+alter table parkshark.parkinglot
+    owner to student;
+
+create table parkshark.membership
+(
+    person_id_fk integer,
+    type         parkshark.membership_type
 );
 
-CREATE TABLE "address" (
-  "address_id" int,
-  "streetname" varchar,
-  "streetnumber" varchar,
-  "city" varchar,
-  "postal_code" varchar
+alter table parkshark.membership
+    owner to student;
+
+create table parkshark.parkinglot_allocation
+(
+    parkinglot_allocation_id integer not null
+        constraint parkinglot_allocation_pk
+            primary key,
+    parkinglot_id_fk         integer
+        constraint parkinglot_id_fk
+            references parkshark.parkinglot,
+    member_id                integer
+        constraint member_id_fk
+            references parkshark.person,
+    license_plate_number     varchar,
+    start_time               varchar,
+    stop_time                varchar
 );
 
-CREATE TABLE "membership" (
-  "person_id_fk" int,
-  "type" membership_type
-);
+alter table parkshark.parkinglot_allocation
+    owner to student;
 
-CREATE TABLE "parkinglot_allocation" (
-  "parkinglot_allocation_id" int,
-  "parkinglot_id_fk" int,
-  "member_id" int,
-  "license_plate_number" varchar,
-  "start_time" varchar,
-  "stop_time" varchar
-);
-
-
-
-ALTER TABLE "parkshark".address ADD FOREIGN KEY ("address_id") REFERENCES "person" ("address_id_fk");
-
-ALTER TABLE "parkshark".person ADD FOREIGN KEY ("id") REFERENCES "parkinglot" ("contact_id_fk");
-
-ALTER TABLE "address" ADD FOREIGN KEY ("address_id") REFERENCES "parkinglot" ("address_id_fk");
-
-ALTER TABLE "division" ADD FOREIGN KEY ("division_id") REFERENCES "division" ("parent_division_id");
-
-ALTER TABLE "person" ADD FOREIGN KEY ("id") REFERENCES "membership" ("person_id_fk");
-
-ALTER TABLE "person" ADD FOREIGN KEY ("id") REFERENCES "division" ("director_id");
-
-ALTER TABLE "person" ADD FOREIGN KEY ("id") REFERENCES "parkinglot_allocation" ("member_id");
-
-ALTER TABLE "parkinglot_allocation" ADD FOREIGN KEY ("parkinglot_id_fk") REFERENCES "parkinglot" ("parkinglot_id");
-
-ALTER TABLE "parkinglot" ADD FOREIGN KEY ("division_id_fk") REFERENCES "division" ("division_id");
+create type parkshark.parking_category as enum ('underground', 'aboveground');
+create type parkshark.membership_type as enum ('Bronze', 'Silver', 'Gold');
