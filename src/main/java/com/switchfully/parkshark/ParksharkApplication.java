1ee1357be5f64138047c12772fa6ee1d305ab0a3
Hans Reygaert <HansRDev@outlook.com>
2021-11-16 16:50:41 +0100

    added db configuration and schema

--- src/main/java/com/switchfully/parkshark/ParksharkApplication.java
+++ src/main/java/com/switchfully/parkshark/ParksharkApplication.java
@@ -1,5 +1,6 @@
 package com.switchfully.parkshark;
 
+import java.time.LocalDate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
