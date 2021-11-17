9087f53215ae103b0da0e9563be2225ada3e5c09
Hans Reygaert <HansRDev@outlook.com>
2021-11-17 11:06:18 +0100

    Changes by Francois.Delepee

--- src/main/java/com/switchfully/parkshark/ParksharkApplication.java
+++ src/main/java/com/switchfully/parkshark/ParksharkApplication.java
@@ -3,7 +3,7 @@ package com.switchfully.parkshark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.switchfully.parkshark", "com.switchsecure", "com.switchfully.homemadesecurity"})
 public class ParksharkApplication {
 
     public static void main(String[] args) {
