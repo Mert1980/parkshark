d038b229790e466b0862dc24cdf64df84dacd3bd
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-11-18 14:44:35 +0100

    Mapper ParkingLot changes

--- src/main/java/com/switchfully/parkshark/services/mapper/ParkingLotMapper.java
+++ src/main/java/com/switchfully/parkshark/services/mapper/ParkingLotMapper.java
@@ -6,8 +6,19 @@ import com.switchfully.parkshark.dto.ParkingLotDtoResponse;
 
 public class ParkingLotMapper {
 
-    public ParkingLot toEntity(ParkingLotDtoRequest parkingLotDtoRequest) {
-        ParkingLot entity = new ParkingLot();
+    private final AddressMapper addressMapper;
+
+    public ParkingLotMapper(AddressMapper addressMapper) {
+        this.addressMapper = addressMapper;
+    }
+
+    public ParkingLot toEntity(ParkingLotDtoRequest request) {
+        ParkingLot entity = ParkingLot.builder()
+                .name(request.getName()   .maxCapacity(request.getMaxCapacity())
+                .pricePerHour(request.getPricePerHour())
+                .address(request.getAddressDtoRequest())
+                .parkingCategory(request.getParkingCategory())
+                .build();
         return entity;
     }
 
