b6ccc49d56c58f1d0da9d7d3f79c9b7389f3e96c
Mert1980 <mertdemirok80@gmail.com>
2021-11-19 11:36:57 +0100

    start testing on ParkingLotService

--- src/test/java/com/switchfully/parkshark/services/ParkingLotServiceTest.java
+++ src/test/java/com/switchfully/parkshark/services/ParkingLotServiceTest.java
@@ -1,19 +1,34 @@
 package com.switchfully.parkshark.services;
 
+import com.switchfully.parkshark.repositories.DivisionRepository;
+import com.switchfully.parkshark.repositories.ParkingLotRepository;
 import com.switchfully.parkshark.repositories.PersonRepository;
+import com.switchfully.parkshark.services.mapper.DivisionMapper;
+import com.switchfully.parkshark.services.mapper.ParkingLotMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotServiceTest {
 
-
-
-    private PersonRepository personRepositoryMock;
+    private DivisionService divisionServiceMock;
+    private PersonService personServiceMock;
+    private ParkingLotRepository parkingLotRepositoryMock;
+    private ParkingLotMapper parkingLotMapperMock;
+    private ParkingLotService parkingLotService;
 
     @BeforeEach
     void setUp() {
-
+        divisionServiceMock = Mockito.mock(DivisionService.class);
+        personServiceMock = Mockito.mock(PersonService.class);
+        parkingLotRepositoryMock = Mockito.mock(ParkingLotRepository.class);
+        parkingLotMapperMock = Mockito.mock(ParkingLotMapper.class);
+        parkingLotService = new ParkingLotService(parkingLotRepositoryMock, parkingLotMapperMock, personServiceMock, divisionServiceMock);
     }
+
+    @Test
+    void whenAddingDivision_thenDivisionRepositorySaveMethodIsCalled() {
 
 }
