25d42e375815faeed8015d27ed1bc2ebabdf24a7
Hans Reygaert <HansRDev@outlook.com>
2021-11-19 12:09:05 +0100

    Multiple validation checks added

--- src/main/java/com/switchfully/parkshark/services/ParkingLotAllocationService.java
+++ src/main/java/com/switchfully/parkshark/services/ParkingLotAllocationService.java
@@ -6,6 +6,8 @@ import com.switchfully.parkshark.domain.Person;
 import com.switchfully.parkshark.dto.ParkingLotAllocationDtoRequest;
 import com.switchfully.parkshark.dto.ParkingLotAllocationDtoResponse;
 import com.switchfully.parkshark.repositories.ParkingLotAllocationRepository;
+import com.switchfully.parkshark.services.exceptions.NotGoldMemberException;
+import com.switchfully.parkshark.services.exceptions.ParkingIsFullException;
 import com.switchfully.parkshark.services.mapper.ParkingLotAllocationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,26 +26,32 @@ public class ParkingLotAllocationService {
   public ParkingLotAllocationService(ParkingLotAllocationRepository parkingLotAllocationRepository,
       ParkingLotAllocationMapper parkingLotAllocationMapper,
       ParkingLotService parkingLotService,
-      PersonService personService){
+      PersonService personService) {
     this.parkingLotAllocationRepository = parkingLotAllocationRepository;
     this.parkingLotAllocationMapper = parkingLotAllocationMapper;
     this.parkingLotService = parkingLotService;
     this.personService = personService;
   }
 
-  public ParkingLotAllocationDtoResponse save(ParkingLotAllocationDtoRequest parkingLotAllocationDtoRequest){
-  //Set person
-    ParkingLotAllocation parkingLotAllocation = parkingLotAllocationMapper.toEntity(parkingLotAllocationDtoRequest);
+  public ParkingLotAllocationDtoResponse save(
+      ParkingLotAllocationDtoRequest parkingLotAllocationDtoRequest) {
+    //Set person
+    ParkingLotAllocation parkingLotAllocation = parkingLotAllocationMapper.toEntity(
+        parkingLotAllocationDtoRequest);
     Person person = personService.findMemberById(parkingLotAllocationDtoRequest.getPersonId());
 
-    return parkingLotAllocationMapper.toResponse(parkingLotAllocationRepository.save(parkingLotAllocation));
+    return parkingLotAllocationMapper.toResponse(
+        parkingLotAllocationRepository.save(parkingLotAllocation));
   }
 
   private void isAllocationRequestValid(ParkingLotAllocationDtoRequest allocationDtoRequest) {
     assertValidPersonId(allocationDtoRequest.getPersonId());
     assertValidParkingLotId(allocationDtoRequest.getParkingLotId());
-    String  personLicensePlateNumber = getLicencePlateNumberFromMember(allocationDtoRequest.getPersonId());
-    assertLicensePlateFromMemberIsTheirs(allocationDtoRequest.getLicensePlateNumber(),personLicensePlateNumber);
+    assertParkingLotIsNotFull(allocationDtoRequest.getParkingLotId());
+    String personLicensePlateNumber = getLicencePlateNumberFromMember(
+        allocationDtoRequest.getPersonId());
+    assertLicensePlateFromMemberIsTheirs(allocationDtoRequest.getLicensePlateNumber(), personLicensePlateNumber);
+
 
   }
 
@@ -51,13 +59,17 @@ public class ParkingLotAllocationService {
     return personService.getMemberById(memberId).getLicencePlateNumber();
   }
 
-  private void assertValidPersonId(long id){
+  private void assertValidPersonId(long id) {
     personService.assertValidPersonId(id);
   }
-  private void assertValidParkingLotId(long id){
+
+  private void assertValidParkingLotId(long id) {
     parkingLotService.assertValidParkingLotId(id);
   }
-  private boolean assertLicensePlateFromMemberIsTheirs(String licensePlate1, String licensePlate2 ){
-    return licensePlate1.equals(licensePlate2);
+
+  private void assertLicensePlateFromMemberIsTheirs(String licensePlate1, String licensePlate2) {
+    if(!licensePlate1.equals(licensePlate2)){
+      throw new NotGoldMemberException();
+    }
   }
 }
