9f7c5ed27c13ade463b3aea322fd0afbc6163367
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-11-19 15:11:22 +0100

    MemberShipLevel feature implemented

--- src/main/java/com/switchfully/parkshark/domain/Person.java
+++ src/main/java/com/switchfully/parkshark/domain/Person.java
@@ -1,16 +1,8 @@
 package com.switchfully.parkshark.domain;
 
 import java.time.LocalDate;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
