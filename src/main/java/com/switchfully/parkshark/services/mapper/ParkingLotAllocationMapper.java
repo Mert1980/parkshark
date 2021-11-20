d53276889dd35c3edd03a41402e12d4436cf7312
Mert1980 <mertdemirok80@gmail.com>
2021-11-20 23:41:52 +0100

    implement user story PSA6

--- src/main/java/com/switchfully/parkshark/services/mapper/ParkingLotAllocationMapper.java
+++ src/main/java/com/switchfully/parkshark/services/mapper/ParkingLotAllocationMapper.java
@@ -5,9 +5,14 @@ import com.switchfully.parkshark.dto.ParkingLotAllocationDtoRequest;
 import com.switchfully.parkshark.dto.ParkingLotAllocationDtoResponse;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
 @Component
 public class ParkingLotAllocationMapper {
-
 
   public ParkingLotAllocation toEntity(ParkingLotAllocationDtoRequest request) {
     return ParkingLotAllocation.builder()
@@ -24,8 +29,34 @@ public class ParkingLotAllocationMapper {
         .parkingLotId(entity.getParkingLotId())
         .startTime(entity.getStartTime())
         .stopTime(entity.getStopTime())
+            .duration(calculateDuration(entity))
         .build();
   }
 
+  private String calculateDuration(ParkingLotAllocation entity) {
+
+    DateTimeFormatter dateTimeFormatter =
+            new DateTimeFormatterBuilder().append(DateTimeFormatter.ISO_LOCAL_DATE)
+                    .appendLiteral(' ')
+                    .appendPattern("HH:mm:ss")
+                    .appendFraction(ChronoField.NANO_OF_SECOND, 1, 9, true)
+                    .toFormatter();
+
+    LocalDateTime startTime = LocalDateTime.parse(entity.getStartTime().replace("T", " "), dateTimeFormatter);
+
+    LocalDateTime stopTime = LocalDateTime.now();
+    if(entity.getStopTime() != null){
+      stopTime = LocalDateTime.parse(entity.getStopTime().replace("T", " "), dateTimeFormatter);
+    }
+
+    Duration duration = Duration.between(startTime, stopTime);
+
+    long days = duration.toDays() % 32;
+    long hours = duration.toHours() % 24 + days * 24;
+    long minutes = duration.toMinutes() % 60;
+    long seconds = duration.getSeconds() % 60;
+
+    return ""+ hours + ":" + minutes + ":" + seconds;
+  }
 
 }
