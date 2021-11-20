423537b3222bcdc5a3b3a0ddc2b710ecf46aa7d1
Hans Reygaert <HansRDev@outlook.com>
2021-11-20 12:25:01 +0100

    import statements were missing, logger too ?

--- src/main/java/com/switchfully/parkshark/api/controllers/ParkingLotAllocationController.java
+++ src/main/java/com/switchfully/parkshark/api/controllers/ParkingLotAllocationController.java
@@ -4,9 +4,6 @@ import com.switchfully.parkshark.dto.ParkingLotAllocationDtoRequest;
 import com.switchfully.parkshark.dto.ParkingLotAllocationDtoResponse;
 import com.switchfully.parkshark.dto.ParkingLotAllocationStopDtoRequest;
 import com.switchfully.parkshark.services.ParkingLotAllocationService;
-
-import java.util.List;
-
 import com.switchsecure.SecurityGuard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,28 +13,36 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/parking-lot-allocations")
 public class ParkingLotAllocationController {
 
-    private final ParkingLotAllocationService parkingLotAllocationService;
-    private final Logger logger = LoggerFactory.getLogger(DivisionController.class);
+  private final ParkingLotAllocationService parkingLotAllocationService;
+  private final Logger logger = LoggerFactory.getLogger(ParkingLotAllocationService.class);
 
-    @Autowired
-    public ParkingLotAllocationController(
-            ParkingLotAllocationService parkingLotAllocationService) {
-        this.parkingLotAllocationService = parkingLotAllocationService;
-    }
+  @Autowired
+  public ParkingLotAllocationController(
+      ParkingLotAllocationService parkingLotAllocationService) {
+    this.parkingLotAllocationService = parkingLotAllocationService;
+  }
 
-    @PostMapping(consumes = "application/json", produces = "application/json")
-    @ResponseStatus(HttpStatus.CREATED)
-    @ResponseBody
-    public ParkingLotAllocationDtoResponse createParkingLotAllocation(@RequestBody ParkingLotAllocationDtoRequest request) {
-        logger.info("Creating parking lot allocation");
-        return parkingLotAllocationService.startParking(request);
-    }
+  @PostMapping(consumes = "application/json", produces = "application/json")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public ParkingLotAllocationDtoResponse createParkingLotAllocation(@RequestBody ParkingLotAllocationDtoRequest request) {
+    logger.info("Creating parking lot allocation");
+    return parkingLotAllocationService.startParking(request);
+  }
 
     @PutMapping(consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
